5cc08d8c76627205482d2a776386cf78010aa0e8
Chris Dabnor <chrisdabnor@Christopher-Dabnors-MacBook-Pro.local>
2019-09-20 16:40:17 +0400

    Added initial build

---
+++ precache-manifest.b132fa54e789c38f0e616c8010f00983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4683cf694bac1095f045d13fb66763b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b89cb99fa4487ed7770a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b768e272edf6317e4492",
+    "url": "/static/js/2.2b44dbcd.chunk.js"
+  },
+  {
+    "revision": "b89cb99fa4487ed7770a",
+    "url": "/static/js/main.79a636ec.chunk.js"
+  },
+  {
+    "revision": "919a9268713a056ccb27",
+    "url": "/static/js/runtime~main.9384d476.js"
+  }
+]);
